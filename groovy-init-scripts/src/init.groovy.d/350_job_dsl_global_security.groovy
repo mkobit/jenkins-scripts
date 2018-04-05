9197077ca7eabfb8871ebdc4753c6e9de3836238
Mike Kobit <mkobit@gmail.com>
2018-04-04 19:08:58 -0500

    Update Job DSL security plugin to enable script security based on updates

--- groovy-init-scripts/src/init.groovy.d/350_job_dsl_global_security.groovy
+++ groovy-init-scripts/src/init.groovy.d/350_job_dsl_global_security.groovy
@@ -4,6 +4,5 @@ import jenkins.model.Jenkins
 final Jenkins jenkins = Jenkins.instance
 
 final GlobalJobDslSecurityConfiguration jobDslSecurity = jenkins.getDescriptorByType(GlobalJobDslSecurityConfiguration)
-// Because we use custom built helper classes that are classloaded with the Job DSL, we need to disable script security.
-jobDslSecurity.useScriptSecurity = false
+jobDslSecurity.useScriptSecurity = true
 jobDslSecurity.save()
